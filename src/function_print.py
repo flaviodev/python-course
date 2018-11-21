238ca8cf81f9f098519391f1be621e364c4dd27c
Flávio de Souza <fdsdev@gmail.com>
2018-11-20 22:07:08 -0200

    finishing class 1

--- src/function_print.py
+++ src/function_print.py
@@ -2,15 +2,15 @@ print("hello world!");
 
 print("using","separator","on","phrase", sep="-");
 
-def printChampion(country,times): 
+def print_champion(country,times): 
     print(country,"wins",times,"world cups", sep=" ", end="\n\n");
 
 country = "Brazil";
 times = 5;
 
-printChampion(country,times);
+print_champion(country,times);
 
 country = "Italy";
 times = 4;
 
-printChampion(country,times);
+print_champion(country,times);
