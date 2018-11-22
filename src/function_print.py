131fc744437c1ef01eec43f968ffdc94b624f647
Flávio de Souza <fdsdev@gmail.com>
2018-11-21 23:46:29 -0200

    removing java accent

--- src/function_print.py
+++ src/function_print.py
@@ -1,16 +1,16 @@
-print("hello world!");
+print("hello world!")
 
-print("using","separator","on","phrase", sep="-");
+print("using","separator","on","phrase", sep="-")
 
 def print_champion(country,times): 
-    print(country,"wins",times,"world cups", sep=" ", end="\n\n");
+    print(country,"wins",times,"world cups", sep=" ", end="\n\n")
 
-country = "Brazil";
-times = 5;
+country = "Brazil"
+times = 5
 
-print_champion(country,times);
+print_champion(country,times)
 
-country = "Italy";
-times = 4;
+country = "Italy"
+times = 4
 
-print_champion(country,times);
+print_champion(country,times)
--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,4 @@
-print("hello world!");
+print("hello world!")
 
 x = 1
 if x == 1:
