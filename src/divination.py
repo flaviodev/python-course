043191f6cd5a3d0ead38910e8a7038bf77814cc9
Flávio de Souza <fdsdev@gmail.com>
2018-11-22 00:04:46 -0200

    class 3 completed

--- src/divination.py
+++ src/divination.py
@@ -3,7 +3,6 @@ print("Welcome to divination game")
 print("---------------------------------------------------------------")
 
 secret_number = 42
-
 str_hint = input("Type you number: ")
 
 print("You typed: ", str_hint)
