229617d3c3993f8b438137effd2911810146d313
Flávio de Souza <fdsdev@gmail.com>
2018-11-22 00:25:26 -0200

    string interpolation, class 4 completed

--- src/divination.py
+++ src/divination.py
@@ -2,24 +2,35 @@ print("---------------------------------------------------------------")
 print("Welcome to divination game")
 print("---------------------------------------------------------------")
 
+total_of_attempts = 3
+usedremaining_attempts = total_of_attempts
+current_attempt = 1
 secret_number = 42
-str_hint = input("Type you number: ")
 
-print("You typed: ", str_hint)
+while(usedremaining_attempts > 0):
+    print("Current attempt: {} of {}".format(current_attempt, total_of_attempts))
+    str_hint = input("Type you number: ")
+    print("You typed: ", str_hint)
 
-try:
-  hint = int(str_hint)
-except:
-  print("You must type a number!")
-  exit()
+    try:
+        hint = int(str_hint)
+    except:
+        print("You must type a number!")
+        exit()
 
-you_are_right = hint == secret_number
-hint_greater_than = hint > secret_number
-hint_less_than = hint < secret_number
+    you_are_right = hint == secret_number
+    hint_greater_than = hint > secret_number
+    hint_less_than = hint < secret_number
 
-if(you_are_right):
-    print("You're right!")
-elif(hint_greater_than):
-    print("The hint is greater than secret number!")
-elif(hint_less_than):
-    print("The hint is less than secret number!")    
+    if(you_are_right):
+        print("You're right!")
+        exit()
+    elif(hint_greater_than):
+        print("The hint is greater than secret number!")
+    elif(hint_less_than):
+        print("The hint is less than secret number!")    
+
+    current_attempt+=1
+    usedremaining_attempts-=1
+
+print("Game over")
