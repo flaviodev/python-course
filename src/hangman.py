87314772389544d6c91a8e3535d6c620d56c2bb0
Flávio de Souza <flavio@syncode.com.br>
2018-12-04 15:28:00 -0200

    Implementing second game

--- src/hangman.py
+++ src/hangman.py
@@ -4,5 +4,13 @@ def play():
     print("Welcome to hangman game")
     print("---------------------------------------------------------------")
 
+    secret_word = 'banana'
+
+    lost = False
+    win = False
+
+    while(not lost and not win): 
+        print('Playing ...')
+
 if(__name__ == "__main__"):
     play()
