704e03f2c2cbe88b220c33ca4e9c8bd796ddffa4
Flávio de Souza <flavio@syncode.com.br>
2018-12-04 17:00:30 -0200

    implementing successful letters. lesson 3 completed

--- src/hangman.py
+++ src/hangman.py
@@ -5,19 +5,29 @@ def play():
     print("---------------------------------------------------------------")
 
     secret_word = 'banana'
+    successful_letters = []
+
+    for letter in secret_word:
+        successful_letters.append('_')
 
     lost = False
     win = False
 
     total_of_attempts = 10
 
-    while(not lost and not win): 
+    while(not lost and not win):
+        print_successful_letters = '' 
+        for letter in successful_letters:
+            print_successful_letters = print_successful_letters + letter + ' '    
+        print(print_successful_letters)
+
         hint = input('What is the letter?')
         hint = hint.strip().upper()
         
         index = 0
         for letter in secret_word.upper():
             if(hint == letter):
+                successful_letters[index] = letter
                 print('Letter \'{}\' found on position {}'.format(letter,index))
             index = index + 1
             
